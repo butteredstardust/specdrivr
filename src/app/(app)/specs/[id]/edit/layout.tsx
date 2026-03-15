f1e9e4a0b950038ffbf1c4d1394e63fe44cb6485
butteredstardust <butteredstardust@gmail.com>
2026-03-15 08:08:15 +0200

    feat: add /specs/[id]/edit editor layout (shell-suppressed) and page

---
+++ src/app/(app)/specs/[id]/edit/layout.tsx
@@ -0,0 +1,7 @@
+export default function EditSpecLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="h-screen bg-[--bg-base]">
+      {children}
+    </div>
+  );
+}
